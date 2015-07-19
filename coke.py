4c1329d8e7b3fb16903800a7b2572f1857502dd8
Ozgur Firat Cinar <ozgurfiratcinar@gmail.com>
2015-07-20 01:15:55 +0300

    coke.py added

---
+++ coke.py
@@ -0,0 +1,11 @@
+formula = """
+    The Secret Formula of Coca-Cola:
+        
+        A wing from one of kedicik's of Adnan Oktar,
+        10 oz mouse blood (helps to cause of chaos),
+        1lt Coca-Cola from mahalle bakkali.
+
+        That's all folks, just mix and feel freshness.
+"""
+
+print formula
